99aea60b762be2f8f8ef50d8aa1bf17c8c30166d
sunwoong-dev <tjsdnd1110@naver.com>
2025-01-08 23:37:21 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.60 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Arrays.sort(array);
+        if (array.length % 2 == 0) {
+            answer = array[array.length / 2 + 1];
+        } else {
+            answer = array[array.length / 2];
+        }
+        return answer;
+    }
+}
